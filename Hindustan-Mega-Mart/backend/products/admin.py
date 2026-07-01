a32cb2971a8d7945d19e1115dadd5908b5f60d20
Ravi Pushp <148735581+ravipushp@users.noreply.github.com>
2026-07-01 12:05:06 +0530

    Customize Django admin for Product model

--- Hindustan-Mega-Mart/backend/products/admin.py
+++ Hindustan-Mega-Mart/backend/products/admin.py
@@ -1,4 +1,28 @@
 from django.contrib import admin
 from .models import Product
 
-admin.site.register(Product)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "name",
+        "price",
+        "stock",
+        "is_active",
+        "created_at",
+    )
+
+    search_fields = (
+        "name",
+        "description",
+    )
+
+    list_filter = (
+        "is_active",
+        "created_at",
+    )
+
+    ordering = (
+        "name",
+    )
