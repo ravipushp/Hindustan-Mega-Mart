72fd0beee4099ff7f72e1e3d3889b18e96099299
Ravi Pushp <148735581+ravipushp@users.noreply.github.com>
2026-07-24 13:06:03 +0530

    Implement dynamic categories section

--- Hindustan-Mega-Mart/backend/products/views.py
+++ Hindustan-Mega-Mart/backend/products/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
-from .models import Product
+from .models import Product, Category
 
 
 def home(request):
 
     products = Product.objects.all()
+    categories = Category.objects.all()
 
     context = {
-        "products": products
+        "products": products,
+        "categories": categories,
     }
 
     return render(request, "products/home.html", context)
