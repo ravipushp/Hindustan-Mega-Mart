3aa34e8e3976a6a7359c49acccd7d87d625e967d
Ravi Pushp <148735581+ravipushp@users.noreply.github.com>
2026-07-24 12:17:05 +0530

    Implement dynamic featured products section

--- Hindustan-Mega-Mart/backend/products/views.py
+++ Hindustan-Mega-Mart/backend/products/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from .models import Product
 
 
 def home(request):
-    return render(request, "products/home.html")
+
+    products = Product.objects.all()
+
+    context = {
+        "products": products
+    }
+
+    return render(request, "products/home.html", context)
