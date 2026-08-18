a5cfc8dd8b8aa86f2a0cfd66d0b9fe57dc3ece24
Ravi Pushp <148735581+ravipushp@users.noreply.github.com>
2026-08-18 19:06:46 +0530

    Add price filtering for products

--- Hindustan-Mega-Mart/backend/products/views.py
+++ Hindustan-Mega-Mart/backend/products/views.py
@@ -8,6 +8,9 @@ def home(request):
     category_slug = request.GET.get("category", "")
     brand_slug = request.GET.get("brand", "")
 
+    min_price = request.GET.get("min_price", "")
+    max_price = request.GET.get("max_price", "")
+
     categories = Category.objects.filter(is_active=True)
     brands = Brand.objects.filter(is_active=True)
     products = Product.objects.filter(is_active=True)
@@ -30,6 +33,18 @@ def home(request):
             brand__slug=brand_slug
         )
 
+    # Minimum Price Filter
+    if min_price:
+        products = products.filter(
+            price__gte=min_price
+        )
+
+    # Maximum Price Filter
+    if max_price:
+        products = products.filter(
+            price__lte=max_price
+        )
+
     context = {
         "products": products,
         "categories": categories,
@@ -37,6 +52,8 @@ def home(request):
         "search_query": search_query,
         "selected_category": category_slug,
         "selected_brand": brand_slug,
+        "min_price": min_price,
+        "max_price": max_price,
     }
 
     return render(request, "products/home.html", context)
